f9d924710d45d9967eed905d8d12867b9af0f0c7
ArturS <arturs1609@gmail.com>
2024-04-09 22:15:21 +0600

    Add README

--- Parser.py
+++ Parser.py
@@ -4,18 +4,18 @@ import urllib3
 
 
 def parse():
-    urllib3.disable_warnings()  # отключаем варнинги чтобы пустило на сайт
+    urllib3.disable_warnings()
     url = 'https://omgtu.ru/general_information/the-structure/the-department-of-university.php'
     page = requests.get(url, verify=False)
     print(page.status_code)  # ответ от страницы
     soup = BeautifulSoup(page.text, "html.parser")
-    block = soup.findAll('div', id="pagecontent")  #находим тег "ul", где нужные нам данные
+    block = soup.findAll('div', id="pagecontent")
     description = ''
     for data in block:
         if data.find('a'):
             descriptiontest = data.text
-            description = descriptiontest.replace('\n\n', '') + '\n'  # записываем с него текст
-    description.strip() #убираем при выводе в файл верхние и нижние сносы строк
+            description = descriptiontest.replace('\n\n', '') + '\n'
+    description.strip() #убираем верхние и нижние сносы строк
     return(description)
 
 
